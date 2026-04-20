17c7d5afd59767e24e72a0eca2915d18242728c6
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-04-20 15:44:23 +0200

    auth update: add login functionality with reactive form, validation, loading state, and session management

--- popsApp/src/app/features/auth/login/login.component.ts
+++ popsApp/src/app/features/auth/login/login.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterLink, Router, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { InputErrorDirective } from '../../../shared/directives/input-error.directive';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [ReactiveFormsModule, InputErrorDirective ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  })
+
+  isLoading = false;
+  errorMessage = '';
+
+  onLogin(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    const {email, password} = this.loginForm.value;
+
+    this.authService.login({email, password}).subscribe({
+      next: (user) => {
+        this.authService.setSession(user);
+        this.isLoading = false;
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.message || 'Login failed. Please try again.';    
+      }
+    });
+
+}
+}
