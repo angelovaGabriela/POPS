52778b8846edeb923c7d85fade2f09ff69aebc7b
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-04-21 14:38:24 +0200

    error page navigating back home

--- popsApp/src/app/features/not-found/not-found.component.ts
+++ popsApp/src/app/features/not-found/not-found.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, Router, RouterLinkActive } from '@angular/router';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
